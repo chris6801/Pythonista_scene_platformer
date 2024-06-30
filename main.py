ffc7c83b2dff3fe1ce4987eb753b0468dba26844
chris6801 <94729095+chris6801@users.noreply.github.com>
2024-06-30 15:11:12 -0700

    Update main.py
    Finally collisions including hot conditions are (mostly) working and logic has been relegated to classes out of the Scene object.

--- main.py
+++ main.py
@@ -46,14 +46,53 @@ walk_textures = [
     
 def clamp(n, smallest, largest):
     return max(smallest, min(n, largest))
-    
+
+class Aura(SpriteNode):
+    def __init__(self, entity, *args, **kwargs):
+        SpriteNode.__init__(self, *args, **kwargs)
+        self.position = entity.position
+        self.z_position = -1
+        self.scale = .6
+        self.count = 0
+        self.anim_idx = 0
+        self.textures = [
+            'shp:Flash00',
+            'shp:Flash01',
+            'shp:Flash02',
+            'shp:Flash03',
+            'shp:Flash04',
+            'shp:Flash05',
+            ]
+            
+        self.texture = Texture(self.textures[0])
+        #self.effect = SpriteNode(self.textures[self.anim_idx])
+        
+        self.blend_mode = BLEND_ADD
+        self.color = '#ff9e9e'
+        
+    def run_effect(self, entity):
+        self.position = entity.position.x, entity.position.y + 25
+        self.texture = Texture(self.textures[self.anim_idx])
+        
+        self.count += 1
+        
+        if self.count > 3:
+            self.anim_idx += 1
+            self.count = 0
+            print('yes')
+        if self.anim_idx > 5:
+            self.anim_idx = 0
+            
+        print('texture: ', self.texture)
+        
 class Button(SpriteNode):
-    def __init__(self, texture, x_pos, y_pos, hit_width, hit_height, *args, **kwargs):
-        SpriteNode.__init__(self, texture, x, y, *args, **kwargs)
+    def __init__(self, texture, x_pos, y_pos, hit_width, hit_height, offset=(0,0), *args, **kwargs):
+        SpriteNode.__init__(self, texture, position=(x_pos, y_pos), anchor_point=(0,0), *args, **kwargs)
         
         self.pressed = False
         
-        self.hit_box = ShapeNode(ui.Path.rect(self, 0, 0, self.frame.w, self.frame.h))
+        self.hit_box = ShapeNode(ui.Path.rect(0, 0, hit_width, hit_height), anchor_point=(0,0), fill_color='clear', stroke_color='green',
+        position=offset)
 
 class PhysicsEntity(SpriteNode):
     def __init__(self, texture, x, y, *args, **kwargs):
@@ -61,40 +100,73 @@ class PhysicsEntity(SpriteNode):
         self.vel = Vector2(0, 0)
         self.accel = Vector2(0, 0)
         self.anchor_point = (0.5, 0)
+        self.collision = False, None, None
+        self.state = 'idle'
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+        self.collided_object = None
        
         self.hit_box = ShapeNode(ui.Path.rect(0, 0, self.frame.w, self.frame.h), position=(0, 50), anchor_point=(0.5,0), fill_color = 'clear', stroke_color='red')
    
         print('frame: ', self.frame)
         print('box frame: ', self.hit_box.frame)
+        centerx = self.anchor_point.x
+        
+    def set_state(self):
+        if self.state == self.old_state:
+            self.state_time += 1
+        else:
+            self.state_time = 0
+            self.anim_idx = 0
+        
     
     def update(self, colliders):
         #collision detection
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+        self.collided_object = None
+        correction = 0
         self.position += self.vel.x, 0
         self.hit_box.position = self.position
         for obj in colliders:
-            if self.hit_box.frame.intersects(obj.frame) and obj != self.hit_box:
+            if self.hit_box.frame.intersects(obj.frame) and obj != self:
+                self.collided_object = obj
                 
                 if self.vel.x < 0:
-                    self.position = obj.frame.max_x + (self.frame.w / 2), self.position.y
+                    self.position = obj.frame.max_x + (self.frame.width/2) + correction, self.position.y
+                    self.collisions['left'] = True
                     
-                if self.vel.x > 0:
-                    self.position = obj.frame.x - self.frame.w + (self.frame.w/2) - 2, self.position.y
+                    
+                elif self.vel.x > 0:
+                    self.position = obj.frame.x - (self.frame.width/2) - correction, self.position.y
+                    self.collisions['right'] = True
+                    
+                else:
+                    self.collision = False, None, None
         
         self.position += 0, self.vel.y
         self.hit_box.position = self.position
         for obj in colliders:
-            if self.hit_box.frame.intersects(obj.frame) and obj != self.hit_box:
+            if self.hit_box.frame.intersects(obj.frame) and obj != self:
+                self.collided_object = obj
                 if self.vel.y < 0:
-                    self.position = self.position.x, obj.frame.max_y
+                    self.position = self.position.x, obj.frame.max_y + correction
                     self.on_ground = True
                     self.jumping = False
+                    self.collisions['down'] = True
                    
-                if self.vel.y > 0:
-                    self.position = self.position.x, obj.frame.y - self.hit_box.frame.h
-                    self.vel.y = 0
+                elif self.vel.y > 0:
+                    self.position = self.position.x, obj.frame.y - self.hit_box.frame.h - correction
+                    self.collisions['up'] = True
                     
+                else:
+                    self.collision = False, None, None
+        if self.collided_object:
+            print('entity: ', self,' collision: ' , self.collisions, ' with: ', self.collided_object)        
+                                   
         #gravity
         self.vel.y = max(GRAVITY, self.vel.y - 0.5)
+        
+        if self.collisions['up'] or self.collisions['down']:
+            self.vel.y = 0
         
     
 class Player(PhysicsEntity):
@@ -103,14 +175,75 @@ class Player(PhysicsEntity):
         
         self.anchor_point = (0.5, 0)
         
-        self.hit_box = ShapeNode(ui.Path.rect(0, 0, 60, 80), anchor_point=(0.5,0), fill_color = 'clear', stroke_color='red')
+        self.hit_box = ShapeNode(ui.Path.rect(0, 0, 60, 80), anchor_point=(0.5,0), fill_color = 'clear', stroke_color='red', z_position=1)
         
         self.jumping = False
         self.on_ground = False
         self.jumping_n = 0
         self.move_touch = None
         self.jump_touch = None
+        
+        self.state = 'idle'
+        self.old_state = None
+        
+        self.state_time = 0
+        self.anim_idx = 0
+        
+        self.effect = None
+        self.aura = None
+        
+        self.textures = { 
+            'idle': [
+                'plf:AlienBlue_front',
+                'plf:AlienBlue_stand'
+                ],
+            'fall': [
+                'plf:AlienBlue_jump'
+                ],
+            'walk': [
+                'plf:AlienBlue_walk1',                 'plf:AlienBlue_walk2'
+                ],
+            'jump': [
+                'shp:Flash00',
+                'shp:Flash01',
+                'shp:Flash02',
+                'shp:Flash03',
+                'shp:Flash04',
+                'shp:Flash05',
+                ]
+                }
     
+    def set_state(self, colliders):
+        if self.on_ground and abs(self.vel.x) > 0:
+            self.state = 'walk'
+        #elif not self.on_ground and self.vel.y < 0:
+            #self.state = 'fall'
+        elif self.on_ground and self.vel.x == 0:
+            self.state = 'idle'
+            
+        elif self.jumping and self.vel.y > 0:
+            self.state = 'jump'
+        
+        #getting hit
+        print(type(self.collided_object))
+        if (self.collisions['up'] or self.collisions['left'] or self.collisions['right']) and type(self.collided_object) == Enemy:
+            self.state = 'hit'
+            print('hit')
+    
+            
+        #print(self.collision)
+            
+        if self.state == self.old_state:
+            self.state_time += 1
+        else:
+            self.state_time = 0
+            self.anim_idx = 0
+            
+        if self.state != self.old_state:
+            if self.old_state == 'jump':
+                self.aura.remove_from_parent()
+                self.aura = None
+        
     def update(self, colliders):
         print('velocity: ', self.vel)
         print('acceleration: ', self.accel)
@@ -118,6 +251,51 @@ class Player(PhysicsEntity):
         PhysicsEntity.update(self, colliders)
         
         print('move, jump: ', self.move_touch, self.jump_touch)
+        
+        self.set_state(colliders)
+        print('player state: ', self.state)
+        match self.state:
+            case 'idle':
+                self.state_time += 1
+                self.texture = Texture(self.textures['idle'][self.anim_idx])
+                
+                if self.state_time > 20:
+                    self.anim_idx += 1
+                    self.state_time = 0
+                if self.anim_idx > 1:
+                    self.anim_idx = 0
+            case 'fall':
+                self.texture = Texture(self.textures['fall'][self.anim_idx])
+            case 'walk':
+                self.state_time += 1
+                self.texture = Texture(self.textures['walk'][self.anim_idx])
+                
+                if self.state_time > 10:
+                    self.anim_idx += 1
+                    self.state_time = 0
+                if self.anim_idx > 1:
+                    self.anim_idx = 0
+                
+            case 'jump':
+                if self.state != self.old_state:
+                    self.aura = Aura(self)
+                    #self.aura.anchor_point = (0.5,0.5)
+                    self.parent.add_child(self.aura)
+                    
+                self.aura.run_effect(self)
+            
+                self.texture = Texture('plf:AlienBlue_jump')
+                
+                #print('aura animation index: ', aura.anim_idx)
+            case 'hit':
+                #self.state_time +=1
+                self.texture = Texture('plf:AlienBeige_hit')
+                
+                
+                if self.state_time > 300:
+                    self.state = 'idle'
+                    
+        self.old_state = self.state
         
         if not self.move_touch or self.jump_touch:
             #print('condition met')
@@ -136,7 +314,6 @@ class Player(PhysicsEntity):
                     
         else:
             self.vel.x += self.accel.x
-                
         
 class Enemy(PhysicsEntity):
     def __init__(self, texture, x, y, *args, **kwargs):
@@ -156,16 +333,27 @@ class Enemy(PhysicsEntity):
             self.state = 'attack'
         elif dist > 100:
             self.state = 'idle'
+            
+        #getting hit
+        print('player type: ', type(self.collided_object))
+        a = self.collisions['up'] == True
+        b = player.collisions['down'] == True
+        c = player.collided_object == self
+        d = type(self.collided_object) == Player
+        if (a or (b and c)) and d:
+            self.state = 'hit'
+            print('hit')
         
     def update(self, player, colliders):
         PhysicsEntity.update(self, colliders)
         
-        self.set_state(player)
         if self.state == self.old_state:
             self.state_time += 1
         else:
             self.state_time = 0
-            
+        
+        self.set_state(player)
+        print('enemy state: ', self.state)
         match self.state:
             case 'idle':
                 self.vel.y = 0
@@ -178,6 +366,9 @@ class Enemy(PhysicsEntity):
                 dist = math.sqrt((self.position.x - player.position.x) ** 2 + (self.position.y - player.position.y) ** 2)
                 self.vel.x = ((self.position.x - player.position.x) * -1) / dist
                 self.vel.y = ((self.position.y - player.position.y) * -1) / dist
+            case 'hit':
+                colliders.remove(self)
+                self.remove_from_parent()
         
         #self.position += self.vel
         self.old_state = self.state
@@ -206,13 +397,18 @@ class MyScene (Scene):
         self.ground = Node(parent=self)
         self.background_color = 'cyan'
         self.offset = [0, 0]
+        
+        self.effect_node = EffectNode(parent=self)
+        self.game_node = Node(parent=self.effect_node)
             
         self.load_map(LEVEL_ONE)
         
+        self.entities = Node(parent=self)
+        
         self.player = Player('plf:AlienBlue_front', 200, 500)
-        self.add_child(self.player)
+        self.entities.add_child(self.player)
         self.add_child(self.player.hit_box)
-        #self.physics_objects.append(self.player.hit_box)
+        self.physics_objects.append(self.player)
         
         self.enemies = Node(parent=self)
         
@@ -221,7 +417,9 @@ class MyScene (Scene):
         
         self.enemy_b = Enemy('plf:Enemy_Fly', 300, 200)
         self.enemies.add_child(self.enemy_b)
-        self.add_child(self.enemy_b.hit_box)
+        #self.add_child(self.enemy_b.hit_box)
+        self.physics_objects.append(self.enemy_a)
+        self.physics_objects.append(self.enemy_b)
         
         self.count = 0
         self.index = 0
@@ -230,19 +428,15 @@ class MyScene (Scene):
         
         self.inputs = []
         
-        self.right_button = SpriteNode('iow:arrow_left_b_256', position = (65,125))
-        self.add_child(self.right_button)
-
-        
-        self.lb_box = ShapeNode(ui.Path.rect(0.00, 0.00, 115.00, 170.00), anchor_point=(0,0), position=(0,40), fill_color = 'clear', stroke_color='green', parent=self)
-        print(self.lb_box.frame)
-        
+        self.left_button = Button('iow:arrow_left_b_256', -65, 0, 115, 170, offset=(0,40))
+        self.add_child(self.left_button)
+        self.add_child(self.left_button.hit_box)
         
         self.right_button = SpriteNode('iow:arrow_right_b_256', position = (175,125))
         self.add_child(self.right_button)
         
         self.rb_box = ShapeNode(ui.Path.rect(0.00, 0.00, 115.00, 170.00), anchor_point=(0,0), position=(120,40), fill_color = 'clear', stroke_color='green', parent=self)
-        print(self.lb_box.frame)
+        #print(self.lb_box.frame)
         
         self.jump_button = SpriteNode('iow:ios7_circle_filled_256', position=(310, 125), scale=.5)
         self.add_child(self.jump_button)
@@ -251,7 +445,7 @@ class MyScene (Scene):
         self.jumping = False
         self.on_ground = False
         self.accel = [0, 0]
-        self.vel = [0, 0]
+        #self.vel = [0, 0]
         
         self.jumping_n = 0
         
@@ -267,7 +461,7 @@ class MyScene (Scene):
         #print(self.physics_objects)
         self.player.update(self.physics_objects)
         
-        self.player.vel.y = max(GRAVITY, self.player.vel.y - 0.5)
+        #self.player.vel.y = max(GRAVITY, self.player.vel.y - 0.5)
         
         #self.vel[0] += self.accel[0] 
         if self.player.vel.x > 5:
@@ -281,43 +475,9 @@ class MyScene (Scene):
             self.player.x_scale = -1
         else:
             self.player.x_scale = 1
-        
-        
-        if self.player.on_ground and abs(self.player.vel.x) > 0:
-            print('true')
-            print(self.walk_idx)
-            self.walk_count += 1
-            self.player.texture = walk_textures[self.walk_idx]
-            
-            if self.walk_count > 3:
-                self.walk_idx += 1
-                self.walk_count = 0
-            if self.walk_idx > 1:
-                self.walk_idx = 0
-                
-             
-        
-        elif self.player.jumping == True:
-            e = SpriteNode(jumping_textures[self.jumping_n], z_position= -1, scale=.4, anchor_point=(0.5,0), parent=self, position=(self.player.position.x , self.player.position.y))
-            e.blend_mode = BLEND_ADD
-            e.color = '#ff3e3e'
-            e.run_action(A.sequence(A.fade_to(0, 0.2), A.remove()))
-            # put somethint that counts time
-            # to slow animation framerate
-            self.count += 1
-            print(self.count)
-            
-            if self.count > 3:
-                self.jumping_n += 1
-                self.count = 0
-                print('yes')
-            if self.jumping_n > 5:
-                self.jumping_n = 0
-                
-            self.player.texture = Texture('plf:AlienBlue_jump')
                     
-        else:
-            self.player.texture = Texture('plf:AlienBlue_front')
+        #else:
+            #self.player.texture = Texture('plf:AlienBlue_front')
             
         #update enemy entities
         for enemy in self.enemies.children:
@@ -352,20 +512,22 @@ class MyScene (Scene):
         self.jump_touch = None
         self.move_touch = None
         self.first_touch = touch.location
-        if self.first_touch in self.lb_box.frame:
+        if self.first_touch in self.left_button.hit_box.frame:
+            self.left_button.pressed = True
+            self.left_button.texture = Texture('iob:arrow_left_b_256')
             self.player.accel.x = -.25
-            self.move_touch = touch.touch_id
+            self.move_touch = touch.touch_id, 'left'
             self.player.move_touch = touch.touch_id
             self.inputs.append(self.move_touch)
         elif self.first_touch in self.rb_box.frame:
             self.player.accel.x = .25
-            self.move_touch = touch.touch_id
+            self.move_touch = touch.touch_id, 'right'
             self.player.move_touch = touch.touch_id
             self.inputs.append(self.move_touch)
         elif self.first_touch in self.jump_button.frame and self.player.on_ground:
             self.player.jump_touch = touch.touch_id
             self.jump_touch = touch.touch_id
-            self.player.vel.y = 25
+            self.player.vel.y = 15
             self.player.on_ground = False
             self.player.jumping = True
             self.inputs.append(self.jump_touch)
@@ -378,14 +540,17 @@ class MyScene (Scene):
     def touch_ended(self, touch):
         if touch.touch_id == self.jump_touch:
             pass
+    
         if self.move_touch or not self.touches:
+            if self.move_touch and self.move_touch[1] == 'left':
+                self.left_button.texture = Texture('iow:arrow_left_b_256')
             if self.player.vel.x > 0:
                 self.player.accel.x = -0.25
             elif self.player.vel.x < 0:
                 self.player.accel.x = 0.25
             elif self.player.vel.x == 0:
                 self.player.accel.x = 0
-            print('accel: ', self.accel[0])
+            #print('accel: ', self.accel[0])
             self.move_touch = None
         
 
